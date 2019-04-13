98288313ffe3c59349c4aafcae03e7e33f2c3168
彭憲奎 <kuei_peng@mayohr.com>
2019-04-13 12:37:04 +0800

    Add project files.

---
+++ NullableConvert/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace NullableConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TestRunner>();
+        }
+    }
+
+    [ClrJob, MonoJob, CoreJob] // 可以針對不同的 CLR 進行測試
+    [MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class TestRunner
+    {
+        private readonly TestClass _test = new TestClass();
+
+        public TestRunner()
+        {
+        }
+
+        [Benchmark]
+        public void TestMethod1() => _test.TestMethod1();
+
+        [Benchmark]
+        public void TestMethod2() => _test.TestMethod2();
+    }
+
+    public class TestClass
+    {
+        private readonly Test _sampleData = new Test{ Id = 1};
+
+        public string TestMethod1()
+        {
+            var result = _sampleData?.Id;
+            return result?.ToString();
+        }
+
+        public string TestMethod2()
+        {
+            return Convert.ToString(_sampleData?.Id);
+        }
+    }
+
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+
+}
